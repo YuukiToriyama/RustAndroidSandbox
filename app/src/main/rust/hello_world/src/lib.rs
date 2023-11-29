7e3a3c6e755484bf29f9191c27acf272e981b6a4
Yuuki Toriyama <github@toriyama.dev>
2023-11-29 21:56:52 +0900

    hello_worldクレートにJNI関数を定義
    MainViewModelから呼び出して使う

--- app/src/main/rust/hello_world/src/lib.rs
+++ app/src/main/rust/hello_world/src/lib.rs
@@ -1,3 +1,20 @@
+use jni::JNIEnv;
+use jni::objects::{JClass, JString};
+use jni::sys::jstring;
+
 pub fn say_hello(name: String) -> String {
     format!("Hello, {}", name)
+}
+
+#[no_mangle]
+pub unsafe extern "system" fn Java_com_example_rustandroidsandbox_MainViewModel_sayHello<'local>(
+    mut env: JNIEnv<'local>,
+    _class: JClass<'local>,
+    input: JString<'local>,
+) -> jstring {
+    let input: String = env.get_string(&input).unwrap().into();
+    let output: JString = env.new_string(
+        say_hello(input)
+    ).unwrap();
+    output.into_raw()
 }
