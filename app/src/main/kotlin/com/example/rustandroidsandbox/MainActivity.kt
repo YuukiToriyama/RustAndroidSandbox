5f2b9cf24c3ddcb262ca34c921a2da8278931f20
Yuuki Toriyama <github@toriyama.dev>
2023-11-29 23:43:17 +0900

    MainActivityからMainViewModel.sayHello()を呼び出す

--- app/src/main/kotlin/com/example/rustandroidsandbox/MainActivity.kt
+++ app/src/main/kotlin/com/example/rustandroidsandbox/MainActivity.kt
@@ -2,19 +2,23 @@ package com.example.rustandroidsandbox
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import com.example.rustandroidsandbox.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var viewModel: MainViewModel
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         // Example of a call to a native method
-        // binding.sampleText.text = stringFromJNI()
+        binding.sampleText.text = viewModel.sayHello("Rust")
     }
 }
